3f4cbe8e053fc5e42f6ff52e0970e6ab31cdcc18
kkcassie <lokadosbtsswag@gmail.com>
2024-09-06 14:48:37 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Hamsters",
+        descricao: "Hamsters são pequenos roedores adoráveis e populares como animais de estimação. Conhecidos por suas bochechas fofinhas e atividades noturnas, esses bichinhos são ideais para quem busca um companheiro de estimação carinhoso e fácil de cuidar. Com suas personalidades curiosas e brincalhonas, os hamsters trazem alegria e diversão para qualquer lar.",
+        link: "https://en.wikipedia.org/wiki/Hamster",
+        tags: "bochechudo pequeno gaiola"
+    },
+    {
+        titulo: "Gatos",
+        descricao: "Gatos são animais de estimação independentes e elegantes, conhecidos por sua agilidade e instinto caçador. Com personalidades distintas, alguns são mais brincalhões, enquanto outros preferem relaxar e observar o ambiente. A relação entre gatos e humanos é marcada pela afeição e companheirismo.",
+        link: "https://en.wikipedia.org/wiki/wiki/Cat",
+        tags: "felino caseiro carinhoso"
+    },
+    {
+        titulo: "Cães",
+        descricao: "Cães são conhecidos como o melhor amigo do homem por sua lealdade e companheirismo. Existem diversas raças, cada uma com suas características físicas e comportamentais. Cães são ótimos para atividades físicas e proporcionam muita alegria aos seus donos.",
+        link: "https://en.wikipedia.org/wiki/Dog",
+        tags: "brincar cachorro cão"
+    }
+];
